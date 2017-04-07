40b09a56984f07f4255512316d1aa5cab7058e02
faith <you@example.com>
2017-04-08 02:43:23 +0530

    create another acticity and it appear after 3 second

--- app/src/main/java/com/example/faith/blogger/MainActivity.java
+++ app/src/main/java/com/example/faith/blogger/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.faith.blogger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends Activity {
 ImageView iv;
@@ -13,5 +17,17 @@ ImageView iv;
         setContentView(R.layout.activity_main);
     iv= (ImageView) findViewById(R.id.imageView3);
         iv.setImageResource(R.drawable.blog);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            public void run() {
+
+                Intent i=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(i);
+                finish();
+
+            }
+
+        }, 3000);
     }
 }
